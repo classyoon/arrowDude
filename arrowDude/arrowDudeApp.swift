c2b8b0fcd5e604c27a67c917ef10407f0458a228
Conner Yoon <con.yoon@icloud.com>
2022-08-27 13:00:31 -0400

    Initial Commit

---
+++ arrowDude/arrowDudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  arrowDudeApp.swift
+//  arrowDude
+//
+//  Created by Conner Yoon on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct arrowDudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
